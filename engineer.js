03dbb75e90fa0cfa297273232aea7a8b267d5ae6
Nikola Lucic <nikola1983@yahoo.com>
2023-02-16 11:45:33 -0500

    created file for eacg employee

---
+++ engineer.js
@@ -0,0 +1,17 @@
+const Employee = require("./employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.gitHub = github;
+  }
+
+  getGitHub() {
+    return this.gitHub;
+  }
+
+  getRole() {
+    return Engineer;
+  }
+}
+module.exports = Engineer;
